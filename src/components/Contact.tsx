529cdb0df339ec07ddb80f504f838467030c6d61
Maruli Gilbert <marulihtgl12@gmail.com>
2025-06-25 19:07:15 +0700

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -127,6 +127,7 @@ const Contact = () => {
                 name="contact"
                 method="POST"
                 data-netlify="true"
+                action="/"
                 className="space-y-6"
               >
                 <input type="hidden" name="form-name" value="contact" />
